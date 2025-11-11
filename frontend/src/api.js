351b0fda50b79670b90b3f46fb78cb892028baee
Sai Viswanth Chirumamilla <viswanthchirumamilla@gmail.com>
2025-11-11 21:31:53 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const API_URL = https://meal-planner-ai-45kt-pvirowgku-sai-viswanths-projects.vercel.app/";
-const VOICEBOT_URL = process.env.REACT_APP_VOICEBOT_URL || "http://127.0.0.1:8001";
 
+const API_URL = "https://meal-planner-ai-6jms.vercel.app/";
+const VOICEBOT_URL =
+  process.env.REACT_APP_VOICEBOT_URL || "http://127.0.0.1:8001";
 const getToken = () => localStorage.getItem("token");
-
 export const axiosInstance = axios.create({
   baseURL: API_URL,
   headers: {
@@ -12,7 +12,7 @@ export const axiosInstance = axios.create({
   },
 });
 
-axiosInstance.interceptors.request.use(config => {
+axiosInstance.interceptors.request.use((config) => {
   const token = getToken();
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
@@ -20,7 +20,7 @@ axiosInstance.interceptors.request.use(config => {
   return config;
 });
 
-// Separate client for Voicebot AI server (mainv.py)
+
 export const axiosVoicebot = axios.create({
   baseURL: VOICEBOT_URL,
   headers: {
@@ -28,7 +28,7 @@ export const axiosVoicebot = axios.create({
   },
 });
 
-axiosVoicebot.interceptors.request.use(config => {
+axiosVoicebot.interceptors.request.use((config) => {
   const token = getToken();
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
